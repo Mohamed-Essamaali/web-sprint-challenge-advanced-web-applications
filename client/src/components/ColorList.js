ab7d442704db87d9c9aae3135256609bf8da9cdc
Mohamed-Essamaali <Mohamed-Essamaali@lambdastudents.com>
2020-11-07 23:06:31 -0500

    added update the colors and delete colors

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import {useParams} from 'react-router-dom'
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -10,6 +12,9 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  // let {id}= useParams();
+  // let activeColor = colors.find(item=>item.id===colorToEdit.id)
+  // console.log('active color id ',activeColor)
 
   const editColor = color => {
     setEditing(true);
@@ -21,10 +26,28 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+    .put(`http://localhost:5000/api/colors/${colorToEdit.id}`,colorToEdit)
+    .then(res=>{
+      updateColors(colors.map(color => 
+        color.id === res.data.id ? res.data : color
+    ))
+    })
+    .catch(err=>{
+      console.log(err)
+    })
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+    .delete(`http://localhost:5000/api/colors/${color.id}`)
+    .then(res => {
+      console.log("delete", res.data)
+      updateColors(colors.filter(color => {
+        return color.id !== res.data
+      }))
+    })
   };
 
   return (
@@ -56,8 +79,9 @@ const ColorList = ({ colors, updateColors }) => {
           <label>
             color name:
             <input
-              onChange={e =>
+              onChange={e =>{
                 setColorToEdit({ ...colorToEdit, color: e.target.value })
+              }
               }
               value={colorToEdit.color}
             />
